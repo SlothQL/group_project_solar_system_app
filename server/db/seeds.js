c6dcae8ecef7d4d5bf78c5088ccbdfe636e1a7d8
Alina <lindenalina.al@gmail.com>
2020-08-09 20:28:56 +0100

    added the first questions into db

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -3,5 +3,24 @@ use knowledge_quiz;
 db.dropDatabase();
 
 db.questions.insertMany([
-    
+    {
+        question: "An Astronomical Unit is the average distance between which two objects in our solar system?",
+        options: ["The Earth and the Moon", "The Earth and the Sun", "The Sun and the Moon", "The Sun and Mars"],
+        correctAnswer: "The Earth and the Sun"
+    },
+    {
+        question: "Ganymede, the largest moon in our solar system, belongs to which planet?",
+        options: ["Neptune", "Uranus", "Jupiter", "Saturn"],
+        correctAnswer: "Jupiter"
+    },
+    {
+        question: "In our solar system, which planet has the shortest day?",
+        options: ["Jupiter", "Venus", "Mars", "The Earth"],
+        correctAnswer: "Jupiter"
+    },
+    {
+        question: "In our solar system which two planets are known as ice giants?",
+        options: ["Neptune and Saturn", "Uranus and Saturn", "Saturn and Pluto", "Neptune and Uranus"],
+        correctAnswer: "Neptune and Uranus"
+    }
 ]);
