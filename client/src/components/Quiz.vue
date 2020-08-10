186a2b79334c53b78f422c2ee59efddb7a4913bb
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-08-10 16:09:13 +0100

    added method to return 10 unique random questions frmo db

--- client/src/components/Quiz.vue
+++ client/src/components/Quiz.vue
@@ -9,14 +9,28 @@ import PlanetService from "../services/PlanetService.js"
 export default {
     data() {
         return {
-            questions: []
+            questions: [],
+            randomQuestions: []
         };
     },
     name: "quiz", 
 
     mounted() {
         PlanetService.getInformation('http://localhost:3000/api/questions/')
-            .then(questions => this.questions = questions);
+            .then(questions => {
+                this.questions = questions
+                this.getRandomQuestions()
+                });
+    },
+    methods: {
+        getRandomQuestions() {
+            while(this.randomQuestions.length < 10) {
+                var randomQuestion = this.questions[Math.floor(Math.random() * this.questions.length)];
+                if (this.randomQuestions.indexOf(randomQuestion) === -1) {
+                    this.randomQuestions.push(randomQuestion)
+                };
+            }
+        }
     }
 
 }
